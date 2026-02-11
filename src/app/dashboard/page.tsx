d12ab7eb10dcc29a2ae8b16f60d457808028af92
Carlos Leme <carlosleme1978@icloud.com>
2026-02-11 17:42:11 +0000

    acertos finais

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,20 +2,7 @@ export const dynamic = "force-dynamic";
 export const revalidate = false;
 
 import DashboardClient from "./DashboardClient";
-import DashboardLiteClient from "./DashboardLiteClient";
 
 export default function Page() {
-  return (
-    <>
-      {/* Desktop / Tablet */}
-      <div className="hidden md:block">
-        <DashboardClient />
-      </div>
-
-      {/* Mobile */}
-      <div className="block md:hidden">
-        <DashboardLiteClient />
-      </div>
-    </>
-  );
+  return <DashboardClient />;
 }
